72e629e489a463dc41701300e4c8e2c30ca84115
honglei <2206812841@qq.com>
2019-01-16 18:43:32 +0800

    add stream summarize

--- src/main/java/com/chenhl/jdk8/StringComparator.java
+++ src/main/java/com/chenhl/jdk8/StringComparator.java
@@ -9,6 +9,7 @@ public class StringComparator {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("zhangsan", "lisi", "wangwu", "zhaoliu");
 
+//        传统⽅方式对字符串串集合排序：
 //        Collections.sort(names, new Comparator<String>() {
 //            @Override
 //            public int compare(String o1, String o2) {
@@ -17,7 +18,7 @@ public class StringComparator {
 //        });
 //        System.out.println(names);
 
-
+//        Lambda方式
         // expression
         Collections.sort(names, (o1, o2) -> o2.compareTo(o1));
         // statement
